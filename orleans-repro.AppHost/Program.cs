c19a6de399c5479c86f498e1399456a0241e55a4
Erik O'Leary <erik.m.oleary@gmail.com>
2024-03-14 10:27:40 -0500

    silo starts

--- orleans-repro.AppHost/Program.cs
+++ orleans-repro.AppHost/Program.cs
@@ -1,3 +1,19 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var storage         = builder.AddAzureStorage("storage").RunAsEmulator();
+var grainStorage    = storage.AddBlobs("grainstate");
+var clusteringTable = storage.AddTables("clustering");
+
+var orleans = builder.AddOrleans("my-app")
+    .WithClustering(clusteringTable)
+    .WithGrainStorage(grainStorage);
+
+var silo = builder.AddProject<orleans_repro_Silo>("silo")
+    .WithReference(orleans);
+
+var client = builder.AddProject<orleans_repro_Client>("client")
+    .WithReference(orleans);
 
 builder.Build().Run();
---
+++ orleans-repro.Silo/Program.cs
@@ -0,0 +1,14 @@
+using Orleans.Runtime;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+builder.AddKeyedAzureTableService("clustering");
+builder.AddKeyedAzureBlobService("grainstate");
+builder.UseOrleans();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
